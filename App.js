e9dde8582858cae8aa69fa4457299d717cdfb28f
Cathal Farrell <cathalproductions@gmail.com>
2022-01-08 15:52:00 +0000

    Added a menu Modal with touchable close button and animation

--- App.js
+++ App.js
@@ -7,11 +7,13 @@ import { Ionicons } from "@expo/vector-icons";
 import { NotificationIcon } from "./components/Icons";
 import Logo from "./components/Logo";
 import Course from "./components/Course";
+import Menu from "./components/Menu";
 
 export default class App extends React.Component {
   render() {
     return (
       <Container>
+        <Menu />
         <SafeAreaView>
           <ScrollView style={{ height: "100%" }}>
             <TitleBar>
---
+++ components/Menu.js
@@ -0,0 +1,75 @@
+import React from "react";
+import styled from "styled-components";
+import { Ionicons } from "@expo/vector-icons";
+import { Animated, TouchableOpacity, Dimensions } from "react-native";
+
+const screenHeight = Dimensions.get("window").height;
+
+class Menu extends React.Component {
+  state = {
+    top: new Animated.Value(screenHeight),
+  };
+
+  componentDidMount() {
+    Animated.spring(this.state.top, { toValue: 0 }).start();
+  }
+
+  toggleMenu = () => {
+    Animated.spring(this.state.top, { toValue: screenHeight }).start();
+  };
+
+  render() {
+    return (
+      <AnimatedContainer style={{ top: this.state.top }}>
+        <Cover />
+        <TouchableOpacity
+          onPress={this.toggleMenu}
+          style={{
+            position: "absolute",
+            top: 120,
+            left: "50%",
+            marginLeft: -22,
+            zIndex: 1,
+          }}
+        >
+          <CloseView>
+            <Ionicons name="ios-close" size={30} color="#546bfb" />
+          </CloseView>
+        </TouchableOpacity>
+        <Content />
+      </AnimatedContainer>
+    );
+  }
+}
+
+export default Menu;
+
+const Container = styled.View`
+  position: absolute;
+  background: white;
+  width: 100%;
+  height: 100%;
+  z-index: 100;
+`;
+
+const AnimatedContainer = Animated.createAnimatedComponent(Container);
+
+const Cover = styled.View`
+  height: 142px;
+  background: black;
+`;
+
+const Content = styled.View`
+  height: ${screenHeight};
+  background: #f0f3f5;
+`;
+
+const CloseView = styled.View`
+  width: 44px;
+  height: 44px;
+  border-radius: 22px;
+  background: white;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.15);
+`;
