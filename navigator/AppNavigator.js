7a8251d7890d4f249802bc6be08abd3ba00a5659
Cathal Farrell <cathalproductions@gmail.com>
2022-01-10 18:21:18 +0000

    Wrap the HomeScreen inside a Navigator

---
+++ navigator/AppNavigator.js
@@ -0,0 +1,9 @@
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import HomeScreen from "../screens/HomeScreen";
+
+const AppNavigator = createStackNavigator({
+  Home: HomeScreen,
+});
+
+export default createAppContainer(AppNavigator);
