9d4c56c1c6b7c56ff7763855d37f0887fc0ba70e
Cathal Farrell <cathalproductions@gmail.com>
2022-01-10 19:16:13 +0000

    Replaced the Navigator with TabNavigator instead (Tabbar)

--- navigator/AppNavigator.js
+++ navigator/AppNavigator.js
@@ -2,6 +2,7 @@ import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 import HomeScreen from "../screens/HomeScreen";
 import SectionScreen from "../screens/SectionScreen";
+import TabNavigator from "./TabNavigator";
 
 const AppNavigator = createStackNavigator(
   {
@@ -11,4 +12,4 @@ const AppNavigator = createStackNavigator(
   { mode: "modal" }
 );
 
-export default createAppContainer(AppNavigator);
+export default createAppContainer(TabNavigator);
